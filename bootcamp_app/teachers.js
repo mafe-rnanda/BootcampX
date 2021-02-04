097c5e571bcaa6034ddb4c1d45ba8474a7d2b785
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-02-04 05:25:39 +0000

    Use node-postgres to query sql through js files

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,20 @@ const pool = new Pool({
 
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers
+  ON teachers.id = teacher_id
+JOIN students
+  ON students.id = student_id
+JOIN cohorts
+  ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+ORDER BY teacher;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort}: ${user.teacher}`);
   })
 }).catch(err => console.error('query error', err.stack));
 
